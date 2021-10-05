0f65eaae0e08960a7f1a05498103991c7256feb2
najun-auto <72716047+najun-auto@users.noreply.github.com>
2021-10-05 14:34:11 +0900

    Update all.py

--- all.py
+++ all.py
@@ -12,8 +12,8 @@ client = AdbClient(host="127.0.0.1", port=5037)
 devices = client.devices()
 device = devices[0]
 
-# app_name = "com.google.android.youtube"
-app_name = "com.miHoYo.GenshinImpact"
+app_name = "com.google.android.youtube"
+# app_name = "com.miHoYo.GenshinImpact"
 cpu_result_file = "cpu_usage"
 gpu_result_file = "gpu_usage"
 fps_result_file = "fps"
@@ -45,12 +45,12 @@ def shell_repeat(num):
         gpu_usage.append(device.shell("cat /sys/devices/platform/18500000.mali/utilization"))
         gpu_result.append(float((gpu_usage[i].split())[0]))
         
-        fps_usage.append(device.shell("dumpsys SurfaceFlinger | grep Log "))
-        temp_data = fps_usage[i].split()
-        numbers = re.sub(r'[^0-9]', '', temp_data[2])
-        fps_result.append(float(numbers))
+        # fps_usage.append(device.shell("dumpsys SurfaceFlinger | grep Log "))
+        # temp_data = fps_usage[i].split()
+        # numbers = re.sub(r'[^0-9]', '', temp_data[2])
+        # fps_result.append(float(numbers))
 
-        network_usage.append(device.shell("cat /proc/net/dev | grep rmnet2| tail -n1"))
+        network_usage.append(device.shell("cat /proc/net/dev | grep rmnet1| tail -n1"))
         recv = (network_usage[i].split())[1]
         send = (network_usage[i].split())[9]
         network_temp = (int(recv) + int(send)) - old_network
@@ -62,20 +62,20 @@ def shell_repeat(num):
     
 def shell_plot(x):
     plt.subplot(221)
-    plt.plot(x, cpu_result, 'o-')
+    plt.plot(x, cpu_result, 'C1')
     plt.title('CPU Usage')
     
     plt.subplot(222)
-    plt.plot(x, gpu_result, '.-')
+    plt.plot(x, gpu_result, 'C2')
     plt.title('GPU Usage')
 
-    plt.subplot(223)
-    plt.plot(x, fps_result, '.-')
-    plt.title('FPS')
+    # plt.subplot(223)
+    # plt.plot(x, fps_result, 'C3')
+    # plt.title('FPS')
 
     plt.subplot(224)
     network_result[0] = 0
-    plt.plot(x, network_result, '.-')
+    plt.plot(x, network_result, 'C4')
     plt.title('network')
 
     plt.show()
