f7ed8c231344cde903d5fc25cce39da812649019
najun-auto <72716047+najun-auto@users.noreply.github.com>
2021-10-14 11:19:38 +0900

    1

--- all.py
+++ all.py
@@ -92,38 +92,38 @@ def shell_repeat(num):
             cpu_result.append((max_cpu_usage-int(numbers))/max_cpu_usage*100)
  
             
-        # if 1:
-        #     ctemp0_usage.append(device.shell("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"))
-        #     ctemp0.append(float((ctemp0_usage[i].split())[0]))
+        if 1:
+            ctemp0_usage.append(device.shell("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"))
+            ctemp0.append(float((ctemp0_usage[i].split())[0]))
 
-        #     # ctemp1_usage.append(device.shell("cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq"))
-        #     # ctemp1.append(float((ctemp1_usage[i].split())[0]))
+            # ctemp1_usage.append(device.shell("cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq"))
+            # ctemp1.append(float((ctemp1_usage[i].split())[0]))
 
-        #     # ctemp2_usage.append(device.shell("cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq"))
-        #     # ctemp2.append(float((ctemp2_usage[i].split())[0]))
+            # ctemp2_usage.append(device.shell("cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq"))
+            # ctemp2.append(float((ctemp2_usage[i].split())[0]))
 
-        #     # ctemp3_usage.append(device.shell("cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq"))
-        #     # ctemp3.append(float((ctemp3_usage[i].split())[0]))
+            # ctemp3_usage.append(device.shell("cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq"))
+            # ctemp3.append(float((ctemp3_usage[i].split())[0]))
 
-        #     ctemp4_usage.append(device.shell("cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_cur_freq"))
-        #     ctemp4.append(float((ctemp4_usage[i].split())[0]))
+            ctemp4_usage.append(device.shell("cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_cur_freq"))
+            ctemp4.append(float((ctemp4_usage[i].split())[0]))
 
-        #     # ctemp5_usage.append(device.shell("cat /sys/devices/system/cpu/cpu5/cpufreq/scaling_cur_freq"))
-        #     # ctemp5.append(float((ctemp5_usage[i].split())[0]))
+            # ctemp5_usage.append(device.shell("cat /sys/devices/system/cpu/cpu5/cpufreq/scaling_cur_freq"))
+            # ctemp5.append(float((ctemp5_usage[i].split())[0]))
 
-        #     # ctemp6_usage.append(device.shell("cat /sys/devices/system/cpu/cpu6/cpufreq/scaling_cur_freq"))
-        #     # ctemp6.append(float((ctemp6_usage[i].split())[0]))
+            # ctemp6_usage.append(device.shell("cat /sys/devices/system/cpu/cpu6/cpufreq/scaling_cur_freq"))
+            # ctemp6.append(float((ctemp6_usage[i].split())[0]))
 
-        #     ctemp7_usage.append(device.shell("cat /sys/devices/system/cpu/cpu7/cpufreq/scaling_cur_freq"))
-        #     ctemp7.append(float((ctemp7_usage[i].split())[0]))
+            ctemp7_usage.append(device.shell("cat /sys/devices/system/cpu/cpu7/cpufreq/scaling_cur_freq"))
+            ctemp7.append(float((ctemp7_usage[i].split())[0]))
 
-        # gpu_usage.append(device.shell("cat /sys/devices/platform/18500000.mali/utilization"))
-        # gpu_result.append(float((gpu_usage[i].split())[0]))
+        gpu_usage.append(device.shell("cat /sys/devices/platform/18500000.mali/utilization"))
+        gpu_result.append(float((gpu_usage[i].split())[0]))
         
-        # # fps_usage.append(device.shell("dumpsys SurfaceFlinger | grep Log "))
-        # # temp_data = fps_usage[i].split()
-        # # numbers = re.sub(r'[^0-9]', '', temp_data[2])
-        # # fps_result.append(float(numbers))
+        fps_usage.append(device.shell("dumpsys SurfaceFlinger | grep Log "))
+        temp_data = fps_usage[i].split()
+        numbers = re.sub(r'[^0-9]', '', temp_data[2])
+        fps_result.append(float(numbers))
 
         # network_usage.append(device.shell("cat /proc/net/dev | grep rmnet1| tail -n1"))
         # recv = (network_usage[i].split())[1]
@@ -138,26 +138,26 @@ def shell_repeat(num):
             temp0_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone0/temp"))
             temp0.append(float((temp0_usage[i].split())[0]))
 
-            temp1_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone1/temp"))
-            temp1.append(float((temp1_usage[i].split())[0]))
+            # temp1_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone1/temp"))
+            # temp1.append(float((temp1_usage[i].split())[0]))
 
-            temp2_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone2/temp"))
-            temp2.append(float((temp2_usage[i].split())[0]))
+            # temp2_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone2/temp"))
+            # temp2.append(float((temp2_usage[i].split())[0]))
 
-            temp3_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone3/temp"))
-            temp3.append(float((temp3_usage[i].split())[0]))
+            # temp3_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone3/temp"))
+            # temp3.append(float((temp3_usage[i].split())[0]))
 
-            temp4_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone4/temp"))
-            temp4.append(float((temp4_usage[i].split())[0]))
+            # temp4_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone4/temp"))
+            # temp4.append(float((temp4_usage[i].split())[0]))
 
-            temp5_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone5/temp"))
-            temp5.append(float((temp5_usage[i].split())[0]))
+            # temp5_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone5/temp"))
+            # temp5.append(float((temp5_usage[i].split())[0]))
 
-            temp6_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone6/temp"))
-            temp6.append(float((temp6_usage[i].split())[0]))
+            # temp6_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone6/temp"))
+            # temp6.append(float((temp6_usage[i].split())[0]))
 
-            temp7_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone7/temp"))
-            temp7.append(float((temp7_usage[i].split())[0]))
+            # temp7_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone7/temp"))
+            # temp7.append(float((temp7_usage[i].split())[0]))
             
             temp8_usage.append(device.shell("su -c cat /sys/class/thermal/thermal_zone8/temp"))
             temp8.append(float((temp8_usage[i].split())[0]))
@@ -167,50 +167,52 @@ def shell_repeat(num):
 
 
 def shell_plot(x):
+
     
-    plt.subplot(211)
+    plt.subplot(511)
+    plt.plot(x, fps_result, 'C3')
+    plt.title('FPS')
+    
+    plt.subplot(512)
     plt.plot(x, cpu_result, 'C1')
     plt.title('CPU Usage')
 
+    plt.subplot(513)
+    plt.plot(x, ctemp0, 'r', label='cpu0')
+    # plt.plot(x, ctemp1, 'r', label='cpu1')
+    # plt.plot(x, ctemp2, 'r', label='cpu2')
+    # plt.plot(x, ctemp3, 'r', label='cpu3')
+    plt.plot(x, ctemp4, 'g', label='cpu4')
+    # plt.plot(x, ctemp5, 'g', label='cpu5')
+    # plt.plot(x, ctemp6, 'g', label='cpu6')
+    plt.plot(x, ctemp7, 'b', label='cpu7')
+    plt.title('CPU Freq')
+    # plt.legend(loc='right')
+    # plt.tight_layout()
 
-    # plt.subplot(413)
-    # plt.plot(x, gpu_result, 'C10')
-    # plt.title('GPU Usage')
+    plt.subplot(514)
+    plt.plot(x, gpu_result, 'C10')
+    plt.title('GPU Usage')
 
-    # # plt.subplot(223)
-    # # plt.plot(x, fps_result, 'C3')
-    # # plt.title('FPS')
 
     # plt.subplot(514)
     # network_result[0] = 0
     # plt.plot(x, network_result, 'C4')
     # plt.title('network')
 
-    plt.subplot(212)
+    plt.subplot(515)
     plt.plot(x, temp0, 'C5', label='temp0')
-    plt.plot(x, temp1, 'C6', label='temp1')
-    plt.plot(x, temp2, 'C7', label='temp2')
-    plt.plot(x, temp3, 'C8', label='temp3')
-    plt.plot(x, temp4, 'C9', label='temp4')
-    plt.plot(x, temp5, 'C10', label='temp5')
-    plt.plot(x, temp6, 'C11', label='temp6')
-    plt.plot(x, temp7, 'C12', label='temp7')
+    # plt.plot(x, temp1, 'C6', label='temp1')
+    # plt.plot(x, temp2, 'C7', label='temp2')
+    # plt.plot(x, temp3, 'C8', label='temp3')
+    # plt.plot(x, temp4, 'C9', label='temp4')
+    # plt.plot(x, temp5, 'C10', label='temp5')
+    # plt.plot(x, temp6, 'C11', label='temp6')
+    # plt.plot(x, temp7, 'C12', label='temp7')
     plt.plot(x, temp8, 'C13', label='temp8')
     plt.title('temp')
-    plt.legend(loc="right")
+    # plt.legend(loc="right")
 
-    # plt.subplot(412)
-    # plt.plot(x, ctemp0, 'r', label='cpu0')
-    # # plt.plot(x, ctemp1, 'r', label='cpu1')
-    # # plt.plot(x, ctemp2, 'r', label='cpu2')
-    # # plt.plot(x, ctemp3, 'r', label='cpu3')
-    # plt.plot(x, ctemp4, 'g', label='cpu4')
-    # # plt.plot(x, ctemp5, 'g', label='cpu5')
-    # # plt.plot(x, ctemp6, 'g', label='cpu6')
-    # plt.plot(x, ctemp7, 'b', label='cpu7')
-    # plt.title('CPU Freq')
-    # plt.legend(loc='right')
-    # # plt.tight_layout()
 
 
     # plt.tight_layout()
